797b25136efdb1a8a91fb3aa5dead8450cfb3084
Rey404 <76277933+Rey404@users.noreply.github.com>
2020-12-31 09:14:13 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,81 @@
+const help = (prefix) => {
+	return 
+	
+	Hello I'm Mr.741L
+	
+	`> *Menu stiker* <
+perintah : *${prefix}sticker* or *${prefix}stiker*
+fungsi : convert gambar/gif/video ke sticker
+caranya : balas atau tag gambar/gif/video, atau kirim gambar/gif/video dengan caption\n
+perintah : *${prefix}sticker nobg* or *${prefix}stiker nobg*
+fungsi : convert image to sticker while removing the background
+caranya : balas atau tag gambar, atau kirim gambar dengan caption\n
+command : *${prefix}toimg*
+desc : convert sticker to image
+usage : reply sticker\n
+command : *${prefix}tsticker* or *${prefix}tstiker*
+desc : convert text to sticker
+usage : *${prefix}tsticker text in here*\n
+> *Menu Meme* <
+command : *${prefix}meme*
+desc : random meme images [english]
+usage : just send the command\n
+command : *${prefix}memeindo*
+desc : random meme images [indo]
+usage : just send the command\n
+> *Others Commands* <
+command : *${prefix}gtts*
+desc : convert text to speech/audio
+usage : *${prefix}gtts [cc] [text]*\nexample : *${prefix}gtts ja On2-chan*\n
+command : *${prefix}loli*
+desc : random loli images
+usage : just send the command\n
+command : *${prefix}nsfwloli*
+desc : random nsfw loli images
+usage : just send the command\n
+command : *${prefix}url2img*
+desc : take web screenshots
+usage : *${prefix}url2img [tipe] [url]*\n
+command : *${prefix}simi*
+desc : your message will be replied to by simi
+usage : *${prefix}simi yourmessage*\n
+command : *${prefix}ocr*
+desc : take the text in the picture
+usage : reply image, or send image with caption\n
+command : *${prefix}wait*
+desc : search anime with image [ What Anime Is This/That ]
+usage : reply image, or send image with caption\n
+command : *${prefix}setprefix*
+desc : replace prefix
+usage : *${prefix}setprefix [text|optional]*\nexample : *${prefix}setprefix ?*
+note : This command can only be used by the bot owner\n
+> *Group Comands* <
+command : *${prefix}linkgroup*
+desc : take the group link
+usage : just send the command\
+note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
+command : *${prefix}tagall*
+desc : tags all group members including admins too
+usage : just send the command
+note : This command can be used if you are a group admin\n
+command : *${prefix}simih*
+desc : activate simi mode in the group
+usage : *${prefix}simih 1* to activate simi mode and *${prefix}simih 0* to deactivate simi mode
+note : This command can be used if you are a group admin\n`
+lainnya 
+#ocr
+#wait
+#bc
+#blcoklist
+#listadmins
+#info
+#clearall
+
+owner : wa.me/6289677763976
+donasi :
+pulsa : 089677763976
+pulsa : 082320355690
+saweria : saweria.co/donate/Rey404
+}
+
+exports.help = help
